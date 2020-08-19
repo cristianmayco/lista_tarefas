9a908198043f441c50879035461c11244da69286
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-08-19 14:45:57 -0300

    Update Check toDo and add to do

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
   List _toDoList = [];
+
+  void addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +47,7 @@ class _HomeState extends State<Home> {
               children: [
                 Expanded(
                     child: TextField(
+                  controller: _toDoController,
                   decoration: InputDecoration(
                       labelText: "Nova tarefa",
                       labelStyle: TextStyle(color: Colors.blueAccent)),
@@ -43,10 +56,30 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: addToDo,
                 )
               ],
             ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    onChanged: (check) {
+                      setState(() {
+                        _toDoList[index]["ok"] = check;
+                      });
+                    },
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                          _toDoList[index]["ok"] ? Icons.check : Icons.error),
+                    ),
+                  );
+                }),
           )
         ],
       ),
